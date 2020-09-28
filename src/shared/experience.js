8ec20fbd194e1842648b30d67fbfd32069c072a8
Akhilkumar31e <akhilkumar31e@gmail.com>
2020-09-28 21:39:42 +0530

    Add Projects

--- src/shared/experience.js
+++ src/shared/experience.js
@@ -12,5 +12,12 @@ export const EXPERIENCE = [
         company:'Scaler Academy',
         time:'March 2020-Present',
         desc:'Leading Scaler developer community in my campus'
+    },
+    {
+        id:2,
+        jobname:'Technical Intern',
+        company:'CodingZap Technologies',
+        time:'August 2020-Present',
+        desc:'Working on back-end and responsible for creating various endpoints for a company project using Node JS and Express Framework.'
     }
 ]
--- src/shared/projects.js
+++ src/shared/projects.js
@@ -3,5 +3,15 @@ export const PROJECTS= [
         id:0,
         name:'Corporatz',
         desc:'An android application useful for corporate employees.Application includes features like Car and Bike pooling,News feed,Corporate Forum,Posting pictures and likes,Ratings and reviews for employers. It is developed using Android Studio and Firebase.'
+    },
+    {
+        id:1,
+        name:'Alumni Tracking System',
+        desc:'A web application developed using React for Frontend , Node Js for Backend with express as middleware and MongoDB as database (MERN Stack).Application helps college management to keep track of their Alumni and thier updates.It also helps to build better connections between alumni.'
+    },
+    {
+        id:2,
+        name:'Society Financial Management',
+        desc:'A web application for housing societies/apartments to manage financial operations typically made in a house society. Technologies used: Servlets, JSP, HTML,CSS,Bootstrap and MySQL for database.'
     }
 ]
